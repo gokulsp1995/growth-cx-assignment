a384d99a7358d094ae21c77ec84d92eae83d6bce
gokulsp1995 <gokulsp2020@outlook.com>
2023-08-26 23:45:21 +0530

    Testimonial component completed

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,15 +3,16 @@ import testimonialsData from "./TestimonialsData";
 const Testimonials = (props) => {
     return(
         <div className="testimonial">
-      <img src={props.image} alt={`${props.name}'s Photo`} className="testimonial-image" />
-      <div className="testimonial-content">
-        <p className="testimonial-text">{props.testimonial}</p>
-        <p className="testimonial-author">
-          {props.name}, {props.position} at {props.company}
-        </p>
-        <p className="testimonial-service">Service: {props.service}</p>
-      </div>
-      <div className="dialogue-pointer"></div>
+        <img src={props.logo} alt={props.logo} className="logo-image" />
+        <div className="testimonial-content">
+          <p className="testimonial-name">{props.name}</p>
+          <p className="testimonial-position">
+            {props.position}
+          </p>
+          <p className="testimonial-content">{props.content}</p>
+          <p className="testimonial-services">Services provided:<br/><span>{props.service}</span></p>
+        </div>
+        <div className="dialogue-pointer"></div>
     </div>
     )
 }
